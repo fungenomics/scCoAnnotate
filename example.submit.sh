34f0d292092f1b7cb35edc9f02ab8bad2ca7f558
alvaannett <46521652+alvaannett@users.noreply.github.com>
2023-09-05 15:59:42 -0400

    Update example.submit.sh

--- example.submit.sh
+++ example.submit.sh
@@ -4,15 +4,15 @@
 #SBATCH --output=logs/%x.out
 #SBATCH --error=logs/%x.err
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=
-#SBATCH --time=
-#SBATCH --mem-per-cpu=
+#SBATCH --cpus-per-task=5
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=60GB 
 
 module load scCoAnnotate/2.0
 
 # path to snakefile and config 
-snakefile=""
-config=""
+snakefile=<path to snakefile>
+config=<path to configfile>
 
 # unlock directory incase of previous errors
 snakemake -s ${snakefile} --configfile ${config} --unlock 

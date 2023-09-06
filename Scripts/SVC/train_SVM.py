ac30121811b9f01c8e957163fb38905b109e22f4
alvaannett <46521652+alvaannett@users.noreply.github.com>
2023-09-06 10:21:02 -0400

    Update train_SVM.py

--- Scripts/SVC/train_SVM.py
+++ Scripts/SVC/train_SVM.py
@@ -5,7 +5,8 @@ Created on Mon Jul 24 18:37:59 2023
 
 @author: tomas.vega
 """
-## Python 3.11.2
+# Python 3.11.2
+
 #--------------- Libraries -------------------
 import numpy as np
 import pandas as pd
@@ -17,7 +18,8 @@ import scanpy as sc
 import pickle
 import os
 import random
-### Set seed
+
+# Set seed
 random.seed(123456) 
 
 #--------------- Parameters -------------------
@@ -41,6 +43,7 @@ labels = pd.read_csv(lab_path,
 
 # check if cell names are in the same order in labels and ref
 order = all(labels.index == ref.index)
+
 # throw error if order is not the same 
 if not order:
   sys.exit("@ Order of cells in reference and labels do not match")
@@ -50,24 +53,24 @@ adata = ad.AnnData(X = ref,
                    var = dict(var_names=ref.columns.astype(str))
                    )
 
-## Now I normalize the matrix with scanpy:
-#Normalize each cell by total counts over all genes,
-#so that every cell has the same total count after normalization.
-#If choosing `target_sum=1e6`, this is CPM normalization
-#1e4 similar as Seurat
+# Now I normalize the matrix with scanpy:
+# Normalize each cell by total counts over all genes,
+# so that every cell has the same total count after normalization.
+# If choosing `target_sum=1e6`, this is CPM normalization
+# 1e4 similar as Seurat
 sc.pp.normalize_total(adata, target_sum=1e4)
-#Logarithmize the data:
+
+# Logarithmize the data:
 sc.pp.log1p(adata)
 
-## Transform labels to array
-
+# Transform labels to array
 label = np.array(labels['label'])
 
 #------------- Train SVC -------------
-##kernel could be ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
-#When the constructor option probability is set to True, class membership
-#probability estimates (from the methods predict_proba and predict_log_proba)
-#are enabled. 
+# kernel could be ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’
+# When the constructor option probability is set to True, class membership
+# probability estimates (from the methods predict_proba and predict_log_proba)
+# are enabled. 
 svm_model = SVC(kernel=classifier,
                 probability=True)
 
@@ -76,13 +79,13 @@ calibrated_model = CalibratedClassifierCV(svm_model,
 calibrated_model.fit(adata.X, label)
 
 
-#Save the model to disk
+# Save the model to disk
 print('@ SAVE MODEL')
 pickle.dump(calibrated_model, open(out_path, 'wb'))
 print('@ DONE')
 
 #------------- Other outputs --------------
-### Plot the tree
+# Plot the tree
 filepath = out_other_path + '/train_parameters.csv'
 print('@ WRITE TRAIN PARAMETERS ')
 params_table = calibrated_model.get_params()
@@ -91,11 +94,11 @@ params_table = pd.DataFrame.from_dict(params_table,
 params_table.to_csv(filepath)
 print('@ DONE')
 
-## I cannot get the features per class, since:
+# I cannot get the features per class, since:
 # there is attribute coef_ for SVM classifier but it only works for SVM with
 # linear kernel. For other kernels it is not possible because data are transformed
 # by kernel method to another space, which is not related to input space
 # (https://stackoverflow.com/questions/21260691/how-to-obtain-features-weights)
 # I cannot get the scores since I use the CalibratedClassifierCV too, 
-#and it doens't have the score attribute.
+# and it doens't have the score attribute.
 

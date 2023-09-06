13c04a0a005626f9a70c53db8f3ccaf66bb10e00
alvaannett <46521652+alvaannett@users.noreply.github.com>
2023-09-06 10:19:01 -0400

    Update predict_SVM.py

--- Scripts/SVC/predict_SVM.py
+++ Scripts/SVC/predict_SVM.py
@@ -17,10 +17,18 @@ import scanpy as sc
 import pickle
 import os
 import random
-### Set seed
+
+# Set seed
 random.seed(123456) 
 
+# Function to get the column name and maximum value for each row
+def get_max_column_and_value(row):
+    pred_label = row.idxmax()
+    proba_label = row.max()
+    return pred_label, proba_label
+
 #--------------- Parameters -------------------
+
 sample_path = str(sys.argv[1])
 model_path = str(sys.argv[2])
 out_path = str(sys.argv[3])
@@ -30,6 +38,7 @@ threads = int(sys.argv[5])
 tool_name = str(sys.argv[6])
 
 #--------------- Data -------------------------
+
 print('@ READ QUERY')
 query = pd.read_csv(sample_path,
                     index_col=0,
@@ -43,15 +52,15 @@ query = ad.AnnData(X = query,
                    var = dict(var_names=query.columns.astype(str))
 )
 
-## Now I normalize the matrix with scanpy:
-#Normalize each cell by total counts over all genes,
-#so that every cell has the same total count after normalization.
-#If choosing `target_sum=1e6`, this is CPM normalization
-#1e4 similar as Seurat
+# Now I normalize the matrix with scanpy:
+# Normalize each cell by total counts over all genes,
+# so that every cell has the same total count after normalization.
+# If choosing `target_sum=1e6`, this is CPM normalization
+# 1e4 similar as Seurat
 sc.pp.normalize_total(query, target_sum=1e4)
+
 #Logarithmize the data:
 sc.pp.log1p(query)
-
 
 # load model 
 print('@ LOAD MODEL')
@@ -59,26 +68,16 @@ SVM_model = pickle.load(open(model_path, 'rb'))
 print('@ DONE')
 
 
-### If the trained model was generated with a diff number of threads and the
-## prediction is done with other number
+# If the trained model was generated with a diff number of threads and the
+# prediction is done with other number
 SVM_model.n_jobs = threads
-## We don't need to calculate again the predict_proba
-#pred = SVM_model.predict(query.X)
+
+# Calculate predicted probability
 pred_proba = SVM_model.predict_proba(query.X)
 df = pd.DataFrame(pred_proba,index = query.obs_names,columns = SVM_model.classes_)
-# Function to get the column name and maximum value for each row
-def get_max_column_and_value(row):
-    pred_label = row.idxmax()
-    proba_label = row.max()
-    return pred_label, proba_label
 
 # Create a new column 'max_column' with the column name containing the maximum value for each row
 df['pred_label'], df['proba_label'] = zip(*df.apply(get_max_column_and_value, axis=1))
-
-#mm1 = pred.tolist()
-#mm = df.max_column.tolist()
-# mm1 == mm True
-## It's the same
 
 # Create a new column 'unknown_max_column' to store 'max_column' as 'unknown' if 'max_value' is lower than the threshold
 df['pred_label_reject'] = df.apply(lambda row: 'Unknown' if row['proba_label'] < threshold else row['pred_label'], axis=1)
@@ -89,7 +88,8 @@ pred_df.to_csv(out_path, index = False)
 print('@ DONE')
 
 #------------- Other outputs --------------
-### Save the prob matrix
+
+# Save the prob matrix
 print('@ WRITTING PROB MATRIX ')
 filename = out_other_path + '/prob_matrix.csv'
 df.to_csv(filename,

48bd2913bb6806cc790ebd59a6ff21ef9f515230
alvaannett <46521652+alvaannett@users.noreply.github.com>
2023-09-06 10:37:55 -0400

    Update run_scNym.py

--- Scripts/scNym/run_scNym.py
+++ Scripts/scNym/run_scNym.py
@@ -6,7 +6,8 @@ import sys
 import scanpy as sc
 import os
 import random
-### Set seed
+
+# Set seed
 random.seed(123456)
 
 #--------------- Parameters -------------------
@@ -30,6 +31,7 @@ labels = pd.read_csv(lab_path,
 
 # check if cell names are in the same order in labels and ref
 order = all(labels.index == ref.index)
+
 # throw error if order is not the same 
 if not order:
   sys.exit("@ Order of cells in reference and labels do not match")
@@ -43,43 +45,48 @@ query = pd.read_csv(sample_path,
 print('@ DONE')
 
 #------------- Preparing input -------------
-## Normalizing reference
+
+#Normalizing reference
 ad_ref = ad.AnnData(X = ref,
                     obs = dict(obs_names=ref.index.astype(str),
                                label = labels['label']),
                     var = dict(var_names=ref.columns.astype(str))
                     )
 
-## Now I normalize the matrix with scanpy:
-#Normalize each cell by total counts over all genes,
-#so that every cell has the same total count after normalization.
-#If choosing `target_sum=1e6`, this is CPM normalization. 
+# Now I normalize the matrix with scanpy:
+# Normalize each cell by total counts over all genes,
+# so that every cell has the same total count after normalization.
+# If choosing `target_sum=1e6`, this is CPM normalization. 
 sc.pp.normalize_total(ad_ref, target_sum=1e6)
-#Logarithmize the data:
+
+# Logarithmize the data:
 sc.pp.log1p(ad_ref)
 sc.pp.filter_genes(ad_ref, min_cells=10)
 
-## Normalizing query
+# Normalizing query
 ad_query = ad.AnnData(X = query,
                       obs = dict(obs_names=query.index.astype(str)),
                       var = dict(var_names=query.columns.astype(str))
 )
 
-## Now I normalize the matrix with scanpy:
-#Normalize each cell by total counts over all genes,
-#so that every cell has the same total count after normalization.
-#If choosing `target_sum=1e6`, this is CPM normalization
+# Now I normalize the matrix with scanpy:
+# Normalize each cell by total counts over all genes,
+# so that every cell has the same total count after normalization.
+# If choosing `target_sum=1e6`, this is CPM normalization
 sc.pp.normalize_total(ad_query, target_sum=1e6)
-#Logarithmize the data:
+
+# Logarithmize the data:
 sc.pp.log1p(ad_query)
 sc.pp.filter_genes(ad_query, min_cells=10)
+
 # Any cell with the annotation "Unlabeled" will be treated as part of the target 
 # dataset and used for semi-supervised and adversarial training.
 ad_query.obs["label"] = "Unlabeled"
 
 
 #------------- Train scNym -------------
-## Contatenate
+
+# Contatenate
 adata = ad_ref.concatenate(ad_query)
 file_model = out_other_path + '/model'
 scnym_api(
@@ -106,7 +113,7 @@ def remove_batch(cell_id, batch):
 
 adata.obs['cell_id'] = adata.obs.index.map(lambda cell_id: remove_batch(cell_id, adata.obs.loc[cell_id, 'batch']))
 
-## Get only the query 
+# Get only the query 
 df = adata.obs[adata.obs["label"] == "Unlabeled"]
 print('@ WRITTING PREDICTIONS')
 pred_df = pd.DataFrame({'cell': df.cell_id, "scNym": df.pred_label_reject})
@@ -114,14 +121,15 @@ pred_df.to_csv(out_path, index = False)
 print('@ DONE')
 
 #------------- Other outputs --------------
-### Save data.frame output
+
+# Save data.frame output
 print('@ WRITTING DATA FRAME OUTPUT ')
 filename = out_other_path + '/whole_df_output.csv'
 df.to_csv(filename,
           index=True) #True because we want to conserve the rownames (cells)
 print('@ DONE ')
 
-#### Save map object that contains the cell x spot prob
+# Save map object that contains the cell x spot prob
 print('@ WRITTING OUTPUT OBJECT ')
 filename = out_other_path + '/scNym_output_object.h5ad'
 adata.write_h5ad(filename= filename,

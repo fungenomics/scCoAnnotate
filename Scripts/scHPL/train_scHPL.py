423d01dfdcd29f741e26cd0e0a385e8ab2f6c33a
alvaannett <46521652+alvaannett@users.noreply.github.com>
2023-09-06 10:33:19 -0400

    Update train_scHPL.py

--- Scripts/scHPL/train_scHPL.py
+++ Scripts/scHPL/train_scHPL.py
@@ -20,7 +20,7 @@ import pickle
 import os
 import random
 
-### Set seed
+# Set seed
 random.seed(123456) 
 
 #--------------- Parameters -------------------
@@ -37,13 +37,14 @@ ref = pd.read_csv(ref_path,
                    index_col=0,
                    sep=',',
                    engine='c') ## could be pyarrow to make it faster but it needs to be install on the module and has many problems with dependencies
-#ref = ref.T
+
 labels = pd.read_csv(lab_path,
                      index_col = 0,
                      sep=',')
 
 # check if cell names are in the same order in labels and ref
 order = all(labels.index == ref.index)
+
 # throw error if order is not the same 
 if not order:
   sys.exit("@ Order of cells in reference and labels do not match")
@@ -53,33 +54,33 @@ adata = ad.AnnData(X = ref,
                    var = dict(var_names=ref.columns.astype(str))
                    )
 
-## Now I normalize the matrix with scanpy:
-#Normalize each cell by total counts over all genes,
-#so that every cell has the same total count after normalization.
-#If choosing `target_sum=1e6`, this is CPM normalization
-#1e4 similar as Seurat
+# Now I normalize the matrix with scanpy:
+# Normalize each cell by total counts over all genes,
+# so that every cell has the same total count after normalization.
+# If choosing `target_sum=1e6`, this is CPM normalization
+# 1e4 similar as Seurat
 sc.pp.normalize_total(adata, target_sum=1e4)
-#Logarithmize the data:
+
+# Logarithmize the data:
 sc.pp.log1p(adata)
 
-### A tree could be use an input, in that case the read_tree can be used.
-### The tree format is Newick formatted file
-#tree = utils.read_tree(treePath)
+# A tree could be use an input, in that case the read_tree can be used.
+# The tree format is Newick formatted file
+# tree = utils.read_tree(treePath)
 
-## They provided this solution here: https://github.com/lcmmichielsen/scHPL/issues/7
+# They provided this solution here: https://github.com/lcmmichielsen/scHPL/issues/7
 tree = utils.create_tree('root')
 tree = learn._construct_tree(tree, labels)
 
-
 #------------- Train scHPL -------------
-##classifier could be svm, svm_occ, knn
-#- the linear SVM when your integrated data still has a lot of
-#dimensions (e.g. when you have used Seurat to integrate the datasets)
-#- the kNN when your integrated data has less, 10-50, dimensions
-#(e.g. when you have used scVI or Harmony to integrate the datasets)
-#- the one-class SVM when your main focus is to find unseen cell
-#populations. A downside of the one-class SVM, however, is that the
-#classification performance drops.
+# classifier could be svm, svm_occ, knn
+# - the linear SVM when your integrated data still has a lot of
+# dimensions (e.g. when you have used Seurat to integrate the datasets)
+# - the kNN when your integrated data has less, 10-50, dimensions
+# (e.g. when you have used scVI or Harmony to integrate the datasets)
+# - the one-class SVM when your main focus is to find unseen cell
+# populations. A downside of the one-class SVM, however, is that the
+# classification performance drops.
 tree = train.train_tree(adata.X,
                         labels.label,
                         tree,
@@ -88,17 +89,16 @@ tree = train.train_tree(adata.X,
                         useRE = True,
                         FN = 0.5)
 
-#Save the model to disk
+# Save the model to disk
 print('@ SAVE MODEL')
 pickle.dump(tree, open(out_path, 'wb'))
 print('@ DONE')
 
 
 #------------- Other outputs --------------
-### Plot the tree
-
-#I'm using this method since they provided it here:
-#https://github.com/lcmmichielsen/scHPL/issues/5
+# Plot the tree
+# I'm using this method since they provided it here:
+# https://github.com/lcmmichielsen/scHPL/issues/5
 def _print_node(node, hor, ver_steps, fig, new_nodes):
     global ver
     # Add horizontal line
